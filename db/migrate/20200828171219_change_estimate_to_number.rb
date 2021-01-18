471a1caefceff8b5a38aa55eda19bf0cfafc0d6a
Phil Riley <rileypb@gmail.com>
2021-01-17 20:02:37 -0500

    fix old migration

--- db/migrate/20200828171219_change_estimate_to_number.rb
+++ db/migrate/20200828171219_change_estimate_to_number.rb
@@ -1,5 +1,5 @@
 class ChangeEstimateToNumber < ActiveRecord::Migration[5.2]
   def change
-  	change_column :issues, :estimate, :integer
+  	change_column :issues, :estimate, 'integer USING CAST(column_name AS integer)'
   end
 end
