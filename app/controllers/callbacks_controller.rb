e17e78e83062c2ebfbcca5eb80570a150ec0f4f2
Phil Riley <rileypb@gmail.com>
2021-01-26 10:13:32 -0500

    Delete callbacks_controller.rb

--- app/controllers/callbacks_controller.rb
+++
@@ -1,8 +0,0 @@
-class CallbacksController < Devise::OmniauthCallbacksController
-	skip_before_action :security_check
-
-    def google_oauth2
-        @user = User.from_omniauth(request.env["omniauth.auth"])
-        sign_in_and_redirect @user
-    end
-end
