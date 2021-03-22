374055eea5547e000b63c277bd59e33b91ac7498
Phil Riley <rileypb@gmail.com>
2021-03-22 10:02:40 -0400

    fix mark completed issues migration

--- db/migrate/20210321192725_mark_completed_issues.rb
+++ db/migrate/20210321192725_mark_completed_issues.rb
@@ -2,7 +2,7 @@ class MarkCompletedIssues < ActiveRecord::Migration[6.1]
   def change
   	Issue.transaction do
   		j = Issue.joins("INNER JOIN sprints ON issues.sprint_id = sprints.id")
-	  	issues = j.where('sprints.completed = true AND issues.state="Closed"')
+	  	issues = j.where("sprints.completed = true AND issues.state='Closed'")
 	  	issues.find_each { |issue| issue.update!(completed: true) }
 	end
   end
