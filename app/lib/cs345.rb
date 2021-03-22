7795609d0816e6a5af8ec0dafbd0d215f8ce81ff
Phil Riley <rileypb@gmail.com>
2021-03-22 11:06:57 -0400

    cs 345 setup helper

---
+++ app/lib/cs345.rb
@@ -0,0 +1,19 @@
+
+class CS345
+	def self.add(last_name, team_number)
+		user = User.find_by_last_name(last_name)
+		if !user
+			puts "No such user"
+			return
+		end
+
+		project = Project.find_by_name("CS345 Sp21 Team #{team_number}")
+		if !team
+			puts "No such project"
+			return
+		end
+
+		ProjectPermission.create!(user: user, project: project, scope: "read")
+		ProjectPermission.create!(user: user, project: project, scope: "update")
+	end
+end
