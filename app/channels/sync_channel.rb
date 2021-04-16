3639f8fcbcd7eede043214385a603502ab78663d
Phil Riley <rileypb@gmail.com>
2021-04-16 15:09:20 -0400

    track and sync on active users

--- app/channels/sync_channel.rb
+++ app/channels/sync_channel.rb
@@ -1,11 +1,15 @@
 
 class SyncChannel < ApplicationCable::Channel
+  @@users = []
+
   def subscribed
     stream_from 'sync'
   end
 
   def unsubscribed
+    @@users.delete_at(@@users.index(current_user.id) || @@users.length)
     stop_all_streams
+    update_users
   end
 
   def receive(data)
@@ -14,10 +18,9 @@ class SyncChannel < ApplicationCable::Channel
 
 
   def sync(data)
-    # need to fix client timing before this will work
-    #if (self.connection.current_user)
+    # if current_user.admin? || data['selector'] != 'users'
       stream_from "sync:#{data['selector']}"
-    #end
+    # end
   end
 
   def cancelsync(data)
@@ -25,8 +28,16 @@ class SyncChannel < ApplicationCable::Channel
   end
 
   def auth(auth_info) 
+    # pp ActionCable.server.connections
     user = User.find_user_for_jwt(auth_info['token'])
     connection.current_user = user
+
+    if (user.admin?)
+      stream_from "sync:users"
+    end
+
+    @@users << current_user.id
+    update_users
   end
 
   def self.broadcast_sync(sync_info) 
@@ -35,6 +46,15 @@ class SyncChannel < ApplicationCable::Channel
     end
   end
 
+  def update_users
+    SyncChannel.broadcast_to 'users', { action: 'sync', selector: 'users', data: user_data}
+  end
 
+  def user_data
+    @@users.uniq.map do |userid|
+      user = User.find(userid)
+      { id: user.id, first_name: user.first_name, last_name: user.last_name, displayName: user.displayName, picture: user.picture }
+    end
+  end
 
 end
